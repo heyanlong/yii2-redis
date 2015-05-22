19071931ed0379fb241aee359c3f2dcd6cc39e4d
何延龙 <yanlong_he@163.com>
2015-05-22 13:23:03 +0800

    commit phpunit case

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,10 +7,10 @@ define('YII_DEBUG', true);
 $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
-require_once(__DIR__ . '/../../vendor/autoload.php');
-require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
-require_once(__DIR__ . '/TestCase.php');
-require_once(__DIR__ . '/../Connection.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+#require_once(__DIR__ . '/TestCase.php');
+#require_once(__DIR__ . '/../Connection.php');
 
 
 Yii::setAlias('@yiiunit/extensions/redis', __DIR__);
